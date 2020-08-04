46a98017d7c88a4b1221a8fe512d8b9cc1703f7c
adtheus <adtheus@student.42.fr>
2020-08-04 19:09:13 +0200

    vivian's contribution :)

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cursus42-Ft_services
+
+##acquerir les differents namespace
+kubectl get namespace 
+##decrire les configmap d'un namespace
+kubectl describe configmap -n metallb-system
+##consulter les explications concernant une ressource:
+kubectl explain pod
+##acquerir l'ip du cluster
+kubectl get node -o=custom-columns='DATA:status.addresses[0].address'
+
--- setup.sh
+++ setup.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
-sudo minikube start
+#sudo usermod -aG docker $(whoami) && newgrp docker 
+minikube start --driver=docker
+
+# see what changes would be made, returns nonzero returncode if different
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl diff -f - -n kube-system
+
+# actually apply the changes, returns nonzero returncode on errors only
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+
 kubectl apply -f ./srcs/Nginx/nginx-deployment.yaml
+kubectl apply -f ./srcs/FTPS/ftps-deployment.yaml
+kubectl apply -f metallb-configmap.yaml
 
