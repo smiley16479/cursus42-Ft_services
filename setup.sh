fd00f3f3d6eea0c345a8d5d2f0734efe01d46c2f
AT <adtheus@student.42.fr>
2020-08-15 17:01:24 +0200

    petit a petit

--- setup.sh
+++ setup.sh
@@ -13,6 +13,14 @@ kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl apply -f - -n kube-system
 
+#To install MetalLB, apply the manifest:
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+docker build ./srcs/FTPS/ -t sftp/my_image >&2
+
 kubectl apply -f ./srcs/Nginx/nginx-deployment.yaml
 kubectl apply -f ./srcs/FTPS/ftps-deployment.yaml
 kubectl apply -f metallb-configmap.yaml
